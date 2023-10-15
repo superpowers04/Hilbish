9a8e6582420c3142b45694600bbb26aab7e68ea8
Super <38338700+superpowers04@users.noreply.github.com>
2023-10-15 01:24:29 -0400

    Fix: cat returns read error instead of throwing a lua error

--- nature/commands/cat.lua
+++ nature/commands/cat.lua
@@ -2,22 +2,28 @@ local commander = require 'commander'
 local fs = require 'fs'
 
 commander.register('cat', function(args, sinks)
-	local exit = 0
 
 	if #args == 0 then
-		sinks.out:writeln [[
-usage: cat [file]...]]
+		sinks.out:writeln "\nusage: cat [file]..."
+		return 0
 	end
 
+	local exit = 0
 	for _, fName in ipairs(args) do
 		local f = io.open(fName)
 		if f == nil then
 			exit = 1
-			sinks.out:writeln(string.format('cat: %s: no such file or directory', fName))
+			sinks.out:writeln(('cat: %s: no such file or directory'):format(fName))
 			goto continue
 		end
-
-		sinks.out:writeln(f:read '*a')
+		local out,err = f:read('*a')
+		if out == nil then
+			exit = 1
+			sinks.out:writeln(('cat: %s: %s'):format(fName,err and err:match(': (.+)') 
+				or 'read returned invalid response' ))
+			goto continue
+		end
+		sinks.out:writeln(out)
 		::continue::
 	end
 	io.flush()
